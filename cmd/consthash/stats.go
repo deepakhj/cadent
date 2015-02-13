e105d0e06ef166453d4c4d88ecfe05d0a077c242
Bo Blanton <boblanton@myfitnesspal.com>
2015-02-13 15:12:41 -0800

    use statsdclient package

--- cmd/consthash/stats.go
+++ cmd/consthash/stats.go
@@ -2,30 +2,13 @@
 package main
 
 import (
-	"./statsd"
-	//"github.com/quipo/statsd"
 	"log"
 	"strconv"
 	"sync/atomic"
 	"time"
+
+	"./statsd"
 )
-
-//impliment a "noop" statsd in case there is no statsd
-type StatsdNoop struct{}
-
-func (s StatsdNoop) CreateSocket() error                                    { return nil }
-func (s StatsdNoop) Close() error                                           { return nil }
-func (s StatsdNoop) Incr(stat string, count int64) error                    { return nil }
-func (s StatsdNoop) Decr(stat string, count int64) error                    { return nil }
-func (s StatsdNoop) Timing(stat string, count int64) error                  { return nil }
-func (s StatsdNoop) PrecisionTiming(stat string, delta time.Duration) error { return nil }
-func (s StatsdNoop) Gauge(stat string, value int64) error                   { return nil }
-func (s StatsdNoop) GaugeDelta(stat string, value int64) error              { return nil }
-func (s StatsdNoop) Absolute(stat string, value int64) error                { return nil }
-func (s StatsdNoop) Total(stat string, value int64) error                   { return nil }
-func (s StatsdNoop) FGauge(stat string, value float64) error                { return nil }
-func (s StatsdNoop) FGaugeDelta(stat string, value float64) error           { return nil }
-func (s StatsdNoop) FAbsolute(stat string, value float64) error             { return nil }
 
 //statsd client singleton
 var StatsdClient statsd.Statsd = nil
@@ -35,7 +18,7 @@ func SetUpStatsdClient(cfg *Config) statsd.Statsd {
 
 	if len(cfg.StatsdServer) == 0 {
 		log.Printf("Skipping Statsd setup, no server specified")
-		StatsdClient = new(StatsdNoop)
+		StatsdClient = new(statsd.StatsdNoop)
 		return StatsdClient
 	}
 	interval := time.Second * 2 // aggregate stats and flush every 2 seconds
