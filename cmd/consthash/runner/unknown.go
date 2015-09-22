6795cba4e5dea5de25cb25548bc3b55441587372
Bo Blanton <boblanton@myfitnesspal.com>
2015-09-22 01:11:16 -0700

    const name

--- cmd/consthash/runner/unknown.go
+++ cmd/consthash/runner/unknown.go
@@ -8,9 +8,11 @@ import (
 	"fmt"
 )
 
+const UNKNOWN_NAME = "unknown"
+
 type UnknownRunner struct{}
 
-func (job *UnknownRunner) Name() (name string) { return "unknown" }
+func (job *UnknownRunner) Name() (name string) { return UNKNOWN_NAME }
 
 func (job UnknownRunner) ProcessLine(line string) (key string, orig_line string, err error) {
 	return "", "", fmt.Errorf("Unknonwn line")
