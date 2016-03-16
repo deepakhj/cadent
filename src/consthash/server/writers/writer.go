a45db4693176be820a8c2a2c540e461ff7b81568
Bo Blanton <boblanton@myfitnesspal.com>
2016-03-15 22:27:12 -0700

    try a "queue" approach to see if we can mitigate some pain

--- src/consthash/server/writers/writer.go
+++ src/consthash/server/writers/writer.go
@@ -11,6 +11,7 @@ import (
 	"consthash/server/writers/indexer"
 	"consthash/server/writers/metrics"
 	"fmt"
+	"sync"
 	//"log"
 	"time"
 )
@@ -93,6 +94,7 @@ type WriterLoop struct {
 	write_chan   chan repr.StatRepr
 	indexer_chan chan repr.StatRepr
 	shutdowner   *broadcast.Broadcaster
+	write_queue  *WriteQueue
 	MetricQLen   int
 	IndexerQLen  int
 }
@@ -103,6 +105,7 @@ func New() (loop *WriterLoop, err error) {
 	loop.IndexerQLen = WRITER_DEFAULT_INDEX_QUEUE_LENGTH
 
 	loop.shutdowner = broadcast.New(1)
+	loop.write_queue = NewWriteQueue()
 	return loop, nil
 }
 
@@ -156,18 +159,43 @@ func (loop *WriterLoop) procLoop() {
 	for {
 		select {
 		case stat := <-loop.write_chan:
-			//go func() {
-			loop.metrics.Write(stat)
-			//loop.indexer.Write(stat.Key)
-			loop.indexer_chan <- stat
-			//return
-			//}() // non-blocking indexer loop
+			/*
+				//go func() {
+				loop.metrics.Write(stat)
+				//loop.indexer.Write(stat.Key)
+				loop.indexer_chan <- stat
+				//return
+				//}() // non-blocking indexer loop
+			*/
+			loop.write_queue.Push(stat)
 		case <-shut.Ch:
 			shut.Close()
 			return
 		}
 	}
 	return
+}
+
+func (loop *WriterLoop) processQueue() {
+
+	proc_queue := func() {
+		ct := loop.write_queue.Len()
+		for i := 0; i < ct; i++ {
+			stat := loop.write_queue.Poll()
+			switch stat {
+			case nil:
+				break
+			default:
+				loop.metrics.Write(*stat)
+				loop.indexer.Write((*stat).Key)
+			}
+		}
+
+	}
+	for {
+		proc_queue()
+		time.Sleep(time.Millisecond)
+	}
 }
 
 func (loop *WriterLoop) Full() bool {
@@ -180,6 +208,7 @@ func (loop *WriterLoop) Start() {
 
 	go loop.indexLoop()
 	go loop.procLoop()
+	go loop.processQueue()
 	go loop.statTick()
 	return
 }
@@ -189,4 +218,92 @@ func (loop *WriterLoop) Stop() {
 		loop.shutdowner.Send(true)
 		return
 	}()
+}
+
+/**** writer "queue" **/
+
+type WriteNode struct {
+	data *repr.StatRepr
+	next *WriteNode
+}
+
+//	A go-routine safe FIFO (first in first out) data stucture.
+type WriteQueue struct {
+	head  *WriteNode
+	tail  *WriteNode
+	count int
+	lock  *sync.Mutex
+}
+
+//	Creates a new pointer to a new queue.
+func NewWriteQueue() *WriteQueue {
+	q := &WriteQueue{}
+	q.lock = &sync.Mutex{}
+	return q
+}
+
+//	Returns the number of elements in the queue (i.e. size/length)
+//	go-routine safe.
+func (q *WriteQueue) Len() int {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+	return q.count
+}
+
+//	Pushes/inserts a value at the end/tail of the queue.
+//	Note: this function does mutate the queue.
+//	go-routine safe.
+func (q *WriteQueue) Push(item repr.StatRepr) {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	n := &WriteNode{data: &item}
+
+	if q.tail == nil {
+		q.tail = n
+		q.head = n
+	} else {
+		q.tail.next = n
+		q.tail = n
+	}
+	q.count++
+}
+
+//	Returns the value at the front of the queue.
+//	i.e. the oldest value in the queue.
+//	Note: this function does mutate the queue.
+//	go-routine safe.
+func (q *WriteQueue) Poll() *repr.StatRepr {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	if q.head == nil {
+		return nil
+	}
+
+	n := q.head
+	q.head = n.next
+
+	if q.head == nil {
+		q.tail = nil
+	}
+	q.count--
+
+	return n.data
+}
+
+//	Returns a read value at the front of the queue.
+//	i.e. the oldest value in the queue.
+//	Note: this function does NOT mutate the queue.
+//	go-routine safe.
+func (q *WriteQueue) Peek() *repr.StatRepr {
+	q.lock.Lock()
+	defer q.lock.Unlock()
+
+	n := q.head
+	if n == nil {
+		return nil
+	}
+
+	return n.data
 }
