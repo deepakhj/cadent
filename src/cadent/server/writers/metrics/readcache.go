326d105e468eb2653bd424db540ade2cbd77f02d
Bo Blanton <boblanton@myfitnesspal.com>
2016-08-15 20:33:33 -0700

    add underarmour header

--- src/cadent/server/writers/metrics/readcache.go
+++ src/cadent/server/writers/metrics/readcache.go
@@ -1,4 +1,20 @@
 /*
+Copyright 2016 Under Armour, Inc.
+ 
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+ 
+http://www.apache.org/licenses/LICENSE-2.0
+ 
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
 	ReadCacher
 
 	For every "metric" read in the API, we add an entry here that's basically a bound list (read_cache_metrics_points)
